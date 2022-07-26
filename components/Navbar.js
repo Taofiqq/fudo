124c3f3dd65b6d82006bb6af412ee681b4862ec4
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2022-07-26 12:46:20 +0800

    navbar responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,35 +3,76 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
+import { FaHamburger } from "react-icons/fa";
+import { useState } from "react";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const quantity = useSelector((state) => state.cart.quantity);
   return (
     <nav className={styles.container}>
       <Link href="/">
         <div className={styles.logoContainer}>
-          <Image src="/logo.png" width={50} height={50} alt="Fudo Logo" />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Image src="/logo.png" width={25} height={25} alt="Fudo Logo" />
+          </div>
           <h1 className={styles.logoTitle}>Fudo</h1>
         </div>
       </Link>
 
-      <Link href="/cart">
-        <div className={styles.contents}>
-          <ul className={styles.contentList}>
-            <Link href="/orders">
-              <li className={styles.contentItem}>Orders</li>
-            </Link>
-            <Link href="/admin">
-              <li className={styles.contentItem}>Admin</li>
-            </Link>
-            <li className={styles.contentItem}>About</li>
-            <li className={styles.contentItem}>Contact</li>
-          </ul>
+      <div className={styles.contents}>
+        <ul className={styles.contentList}>
+          <li className={styles.contentItem}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.contentItem}>
+            <Link href="/orders">Orders</Link>
+          </li>
+          <li className={styles.contentItem}>
+            <Link href="/admin">Admin</Link>
+          </li>
+          <li className={styles.contentItem}>Contact</li>
+        </ul>
+
+        <Link href="/cart">
           <div className={styles.cart}>
+            <span className={styles.cartQuantity}>{quantity}</span>
             <AiOutlineShoppingCart className={styles.cartIcon} />
-            <div className={styles.cartQuantity}>{quantity}</div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
+      <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+        <FaHamburger />
+      </div>
+
+      <ul
+        className={styles.mobileMenu}
+        onClick={() => setIsOpen(false)}
+        style={{
+          right: isOpen ? "0" : "-100%",
+        }}
+      >
+        <li className={styles.menuItem}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/orders">Orders</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/admin">Admin</Link>
+        </li>
+        <li className={styles.menuItem}>Contact</li>
+        <Link href="/cart">
+          <div className={styles.cartMobile}>
+            <span className={styles.cartQuantityMobile}>{quantity}</span>
+            <AiOutlineShoppingCart className={styles.cartIcon} />
+          </div>
+        </Link>
+      </ul>
     </nav>
   );
 };
